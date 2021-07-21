d6a551385eb855f02bfcb1996329ad8ecb29afe1
chandu movva <chandumovva4@gmail.com>
2021-07-22 01:23:02 +1200

    completed game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Status from "./Components/Status";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: Array(9).fill(null),
+      player: "X",
+      winner: null,
+    };
+  }
+  checkWinner() {
+    let winLines = [
+      ["0", "1", "2"],
+      ["3", "4", "5"],
+      ["6", "7", "8"],
+      ["0", "3", "6"],
+      ["1", "4", "7"],
+      ["2", "5", "8"],
+      ["0", "4", "8"],
+      ["2", "4", "6"],
+    ];
+
+    for (let index = 0; index < winLines.length; index++) {
+      const [a, b, c] = winLines[index];
+      if (
+        this.state.board[a] &&
+        this.state.board[a] === this.state.board[b] &&
+        this.state.board[a] === this.state.board[c]
+      ) {
+        alert("you Won");
+        this.setState({
+          winner: this.state.player,
+        });
+      }
+    }
+  }
+  handleClick(index) {
+    if (this.state.player && !this.state.winner) {
+      let newBoard = this.state.board;
+      if (this.state.board[index] === null) {
+        newBoard[index] = this.state.player;
+      }
+
+      this.setState({
+        board: newBoard,
+        player: this.state.player === "X" ? "0" : "X",
+      });
+      this.checkWinner();
+    }
+  }
+
+  setPlayer(player) {
+    this.setState({
+      player,
+    });
+  }
+
+  renderBoxes() {
+    return this.state.board.map((box, index) => (
+      <div className="box" key={index} onClick={() => this.handleClick(index)}>
+        {box}
+      </div>
+    ));
+  }
+  handleReset() {
+    this.setState({
+      winner: null,
+      player: null,
+    });
+  }
+  render() {
+    return (
+      <div className="container">
+        <h2>Tic Tac Toe</h2>
+
+        <Status
+          player={this.state.player}
+          setPlayer={(e) => {
+            this.setPlayer(e);
+          }}
+          winner={this.state.winner}
+        />
+        <div className="board">{this.renderBoxes()}</div>
+        <button
+          disabled={!this.state.winner}
+          onClick={() => this.handleReset()}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Reset
+        </button>
+      </div>
+    );
+  }
 }
-
 export default App;
